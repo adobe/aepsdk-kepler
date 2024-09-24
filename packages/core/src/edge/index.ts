290389731879597098711afc27b489d4fb80cd5a
Arjun Bhadra <abhadra@adobe.com>
2024-09-24 09:16:03 -0700

    Added Edge, Identity and Consent base code

---
+++ packages/core/src/edge/index.ts
@@ -0,0 +1,16 @@
+import { Extension } from "../core/extension";
+import { EdgeImpl } from "./EdgeImpl";
+
+// Interface
+export interface Edge extends Extension {
+    // public APIs
+
+    // sendEvent(xdm: Map<string, object>, data: Map<string, object>): void;
+    // sendEventWithPromise(xdm: Map<string, object>, data: Map<string, object>): Promise<any>;
+
+    // getECID(): string | null;
+
+    //setConsent(consent: Map<string, object>): void;
+}
+
+export const edge: Edge = new EdgeImpl();
